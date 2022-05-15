d89b5fc765a2286112cd4c2d7cb232f733036687
PAILA MADHU <56785712+Maddy123-hack@users.noreply.github.com>
2022-05-15 11:26:57 +0530

    Add files via upload

---
+++ text_to_handwriting.py
@@ -0,0 +1,18 @@
+import pywhatkit
+assignment = """
+<<REGRESSION>>
+Regression is a statistical method used in finance, investing, and other disciplines that attempts to determine the strength and character of the relationship between one dependent variable (usually denoted by Y) and a series of other variables (known as independent variables).
+
+
+
+
+<<TYPES OF REGRESSION>>
+Types of Regression Analysis Techniques
+1. Linear Regression
+2. Logistic Regression
+3. Ridge Regression
+4. Lasso Regression
+5. Polynomial Regression
+6. Bayesian Linear Regression
+"""
+pywhatkit.text_to_handwriting(assignment, save_to="19021A0528.jpg")
